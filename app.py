1d72065f0cc0cc78d2af941a4cafc33c47f0751a
Ridvan Chasan <36571711+rich-info@users.noreply.github.com>
2022-02-22 20:56:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+  
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello, {escape(name)}!"
+  
+ 
+@app.route('/duplicate/<int:my_number>')
+def duplicate_number(my_number):
+    return f'2 x {my_number} = {my_number * 2}'
